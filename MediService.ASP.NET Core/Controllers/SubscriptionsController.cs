5939ce257153b01669978139c5d3feb368278771
Vondai <62604050+Vondai@users.noreply.github.com>
2021-07-04 10:58:12 +0300

    Added navigation bar buttons for register/login/logout. Added mock pages for Team/Subscription. Added controllers for subscription and team.

---
+++ MediService.ASP.NET Core/Controllers/SubscriptionsController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MediService.ASP.NET_Core.Controllers
+{
+    public class SubscriptionsController : Controller
+    {
+        public IActionResult All() => View();
+    }
+}
